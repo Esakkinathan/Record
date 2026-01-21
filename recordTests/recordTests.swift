ddfae39fd197f76dfec985cb8782829c98ea2da3
Esakkinathan <esakkibes91@gmail.com>
2026-01-21 11:50:58 +0530

    Initial Commit

---
+++ recordTests/recordTests.swift
@@ -0,0 +1,17 @@
+//
+//  recordTests.swift
+//  recordTests
+//
+//  Created by Esakkinathan B on 21/01/26.
+//
+
+import Testing
+@testable import record
+
+struct recordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
