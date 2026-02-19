c6a88871b51db5615be26b08046a80cc4ce79bd9
Esakkinathan <esakkibes91@gmail.com>
2026-02-19 23:42:37 +0530

    Clean minimal project

--- Pods/Target Support Files/Pods-record-recordUITests/Pods-record-recordUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_record_recordUITests : NSObject
-@end
-@implementation PodsDummy_Pods_record_recordUITests
-@end
--- Pods/Target Support Files/Pods-record/Pods-record-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_record : NSObject
-@end
-@implementation PodsDummy_Pods_record
-@end
